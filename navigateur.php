61ccdd76f60d8cf52f4d313a588c78ba70b22163
Hifi58 <gillet.tony@gmail.com>
2021-04-21 14:23:55 +0200

    V1 Portfolio

---
+++ navigateur.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="navigation.css">
+
+<nav>
+<div class="logo">
+</div>
+<div class="container">
+        <div class ="Liens">
+            <ul>
+                <li><a href="#accueil">Home</a></li>
+                <li><a href="#skill">Skills</a></li>
+                <li><a href="#experience">Project</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
