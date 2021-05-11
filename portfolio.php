dcd9b255bb2ce4ba6d9a8bc5fa3a236c3f583c32
Hifi58 <gillet.tony@gmail.com>
2021-05-11 16:29:39 +0200

    modif

--- portfolio.php
+++ portfolio.php
@@ -10,7 +10,26 @@
 
 
 <body>
-    <?php include 'navigateur.php' ?>
+<nav>
+
+<div class="logo">
+    <img href="images/tg.gif" alt="logo">
+</div>
+<div class="container">
+        <div class ="Liens">
+            <div class="wrapper">
+            <ul>
+                <li><a href="#accueil">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#work">Work</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+            </div>
+        </div>
+        
+    </div>  
+</nav>
 
     <header>
        
