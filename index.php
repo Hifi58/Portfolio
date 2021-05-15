1c8efb93e4b919ea8804bd84f95dc542e7168200
Hifi58 <gillet.tony@gmail.com>
2021-05-15 15:34:37 +0200

    ajout d'animation en scroll avec AOS

--- index.php
+++ index.php
@@ -4,34 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <link rel="stylesheet" href="style.css">
     <!-- <link rel="icon" href="images/logo.gif" /> -->
     <title>Tony Gillet</title>
 </head>
 <body>
 
-<div class="background">
-   <span></span>
-   <span></span>
-   <span></span>
-   <span></span>
-   <span></span>
-   <span></span>
-   <span></span>
-   <span></span>
-   <span></span>
-   <span></span>
-   <span></span>
-   <span></span>
-   <span></span>
-   <span></span>
-   <span></span>
-   <span></span>
-   <span></span>
-   <span></span>
-   <span></span>
-   <span></span>
-</div>
 
 <!----------------------------------
 
@@ -39,7 +18,7 @@ About
 
 ----------------------------------->
 
-   <section class="about">
+   <section class="about" >
         <div class="containerabout">
             <div class="titleabout">
                 <h1 class="titletony">Tony Gillet,<br/> développeur Web / Web mobile Fullstack</h1>
@@ -51,13 +30,14 @@ About
         </div>
    </section>
 
+
 <!----------------------------------
 
 Work
 
 ----------------------------------->
 
-   <section class="work">
+   <section class="work" >
         <div class="containerwork">
             <div class="titlework">
                 <h1 class="titleproject">Les projets que j'ai réalisé</h1>
@@ -72,9 +52,18 @@ Skills
 ----------------------------------->
 
    <section class="skills">
-        <div class="containerskills">
+        <div class="containerskills" data-aos="zoom-in">
             <div class="titleskills">
                 <h1 class="titlescomp">Mes compétences</h1>
+                <div class="imgskills">
+                    <img src="images/icones/html-5.png"/>
+                    <img src="images/icones/css.png"/>
+                    <img src="images/icones/javascript.png"/>
+                    <img src="images/icones/sass.png"/>
+                    <img src="images/icones/php.png"/>
+                    <img src="images/icones/mysql.png"/>
+                    <img src="images/icones/github.png"/>
+                </div>
             </div>
         </div>
    </section>
@@ -86,13 +75,26 @@ Contact
 
 ----------------------------------->
 
-   <section class="contact">
+   <section class="contact" >
         <div class="containercontact">
-            <div class="titlecontact">
-                <h1 class="titleform">formulaire contact</h1>
+            <div class="titlecontact" data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-offset="300">
+                <h1 class="titleform">Me contacter</h1>
+
+                <form action="mailto:t.gillet@codeur.online" method="post">
+
+                <input type="text" placeholder="Votre nom"><br/>
+                <input type="text" placeholder="Votre E-mail"><br/>
+                <input class="message" type="text" placeholder="Écrivez votre message ici..." size="50"><br>
+                <button type="submit" value="envoyer">Envoyer</button>
+                </form>
             </div>
         </div>
    </section>
 
+  
+   <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+   <script>
+    AOS.init();
+  </script>
 </body>
 </html>
