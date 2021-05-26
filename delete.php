e5020d9c232cf8f86494d59095d7ea8c6d5ab344
Hifi58 <gillet.tony@gmail.com>
2021-05-26 16:53:12 +0200

    Update

--- delete.php
+++ delete.php
@@ -5,12 +5,4 @@ $id = $_GET['id_projet'];
 
 $bdd->exec("DELETE FROM projets WHERE id_projet = $id ");
 
-echo '<div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-<div class="toast-body">
-  Hello, world! This is a toast message.
-  <div class="mt-2 pt-2 border-top">
-    <button type="button" class="btn btn-primary btn-sm">Valider</button>
-    <button type="button" class="btn btn-secondary btn-sm" data-bs-dismiss="toast">Annuler</button>
-  </div>
-</div>
-</div>';
+var_dump($id);
