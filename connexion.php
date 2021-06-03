c708b8de753e19e6d452e506061721717b155056
Hifi58 <78985131+Hifi58@users.noreply.github.com>
2021-06-03 11:38:59 +0200

    Delete connexion.php

--- connexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-try{
-$bdd = new PDO('mysql:host=localhost;dbname=tonyg_portfolio;charset=utf8', 'tonyg', 'syfd05x88Q7GqA==');
-}
-catch (Exception $e)
-{
-        die('Erreur : ' . $e->getMessage());
-}
